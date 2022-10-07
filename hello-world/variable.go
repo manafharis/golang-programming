904e3e7fc7e91c930d793a57efc22e2abf3e690a
manafharis <harismanaf96@gmail.com>
2022-10-07 23:57:03 +0700

    first commit

---
+++ hello-world/variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "john"
+
+	var lastName string
+	lastName = "wick"
+
+	first, second, thrird := "Satu", "Dua", "Tiga"
+
+	say, _ := "Haris Manaf Kamu pasti Bisa", "Bismillahj"
+
+	fmt.Printf("hallo %s %s! \n", firstName, lastName)
+	fmt.Println(first, second, thrird)
+	fmt.Print(say)
+}
