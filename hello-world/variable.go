7293b5bf4fee0e06961752e28923b520fb8ece1e
manafharis <harismanaf96@gmail.com>
2022-10-08 00:17:58 +0700

    second commit

--- hello-world/variable.go
+++ hello-world/variable.go
@@ -11,8 +11,10 @@ func main() {
 	first, second, thrird := "Satu", "Dua", "Tiga"
 
 	say, _ := "Haris Manaf Kamu pasti Bisa", "Bismillahj"
+	istri := "Nur Annisya Cantik"
 
 	fmt.Printf("hallo %s %s! \n", firstName, lastName)
 	fmt.Println(first, second, thrird)
 	fmt.Print(say)
+	fmt.Println("\n", istri)
 }
